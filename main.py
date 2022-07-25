805e645762c78e67c02c0bc286d95ca4cba58576
John Gruber <j.gruber@f5.com>
2022-07-25 14:37:37 -0500

    added debuging on client IPs and header matching

--- main.py
+++ main.py
@@ -28,10 +28,14 @@ from pydantic import BaseModel, Field, ValidationError, validator
 DEFAULT_CONFIG_FILE: str = "%s/config.yaml" % os.path.dirname(
     os.path.realpath(__file__))
 DEFAULT_POLICIES: List[Any] = [{
-    'src_cidr': 'ALL',
-    'path_re_match': 'ALL',
-    'method': 'ALL',
-    'header': 'NONE',
+    'src_cidr':
+    'ALL',
+    'path_re_match':
+    'ALL',
+    'method':
+    'ALL',
+    'header':
+    'NONE',
     'ip_version':
     4,
     'reply_scripts': [{
@@ -76,24 +80,24 @@ class RunScript(BaseModel):
     inject_headers: Union[List[Mapping[str, str]], None] = Field(
         default=[],
         title='Headers to Inject',
-        description='Headers to inject in the response'
-    )
+        description='Headers to inject in the response')
     action: Union[str, None] = Field(
         default='direct_response',
         title='Script action',
-        description='What action to take: direct_response, redirect or serve_local'
-    )
+        description=
+        'What action to take: direct_response, redirect or serve_local')
     direct_response_status_code: Union[int, None] = Field(
         default=200,
         title='HTTP response status code',
         description='The HTTP status code to return')
-    direct_response_mime_type: Union[str,
-                     None] = Field(default='application/json',
-                                   title='MIME type to return',
-                                   description='MIME type of the return body')
-    direct_response_body: Union[str, None] = Field(default='{}',
-                                   title='Body content',
-                                   description='Response body content')
+    direct_response_mime_type: Union[str, None] = Field(
+        default='application/json',
+        title='MIME type to return',
+        description='MIME type of the return body')
+    direct_response_body: Union[str, None] = Field(
+        default='{}',
+        title='Body content',
+        description='Response body content')
     direct_response_body_encoding: Union[str, None] = Field(
         default='NONE',
         title='Body content encoding',
@@ -103,21 +107,15 @@ class RunScript(BaseModel):
     redirect_status_code: Union[str, None] = Field(
         default=307,
         title='Redirect Status Code',
-        description=
-        'Which redirect status code to use 301,302,307'
-    )
+        description='Which redirect status code to use 301,302,307')
     redirect_url: Union[str, None] = Field(
         default='NONE',
         title='Redirect URL',
-        description=
-        'URL to redirect the request'
-    )
+        description='URL to redirect the request')
     serve_local_file_path: Union[str, None] = Field(
         default='NONE',
         title='Serve Local File',
-        description=
-        'Local file path for the file to serve for the request'
-    )
+        description='Local file path for the file to serve for the request')
 
     @validator('action')
     def action_needs_to_be_in_supported_actions(cls, v):
@@ -156,6 +154,7 @@ class RunScript(BaseModel):
                 status_code=400,
                 detail='redirect_status_code should be 301,302 or 307')
         return v
+
 
 class Policy(BaseModel):
     id: Union[int, None] = Field(
@@ -351,8 +350,8 @@ def run_redirect(script, request, response):
     redirect_status_code = 307
     if 'redirect_status_code' in script:
         redirect_status_code = script['redirect_status_code']
-    response.headers['location'] =  urllibparse.quote(
-            script['redirect_url'], safe=":/%#?=@[]!$&'()*+,;")
+    response.headers['location'] = urllibparse.quote(
+        script['redirect_url'], safe=":/%#?=@[]!$&'()*+,;")
     return Response(content=None,
                     status_code=redirect_status_code,
                     headers=response.headers)
@@ -386,13 +385,7 @@ def run_script(script, request, response):
 
 
 def get_client_ip_match(client_ips):
-    client_match_policies = {
-        'ALL': [],
-        'CIDR': {
-            'hosts': 0,
-            'hashes': []
-        }
-    }
+    client_match_policies = {'ALL': [], 'CIDR': {'hosts': 0, 'hashes': []}}
     for policy_hash in policies:
         policy = policies[policy_hash]
         if policy['src_cidr'] == 'ALL':
@@ -402,42 +395,56 @@ def get_client_ip_match(client_ips):
             for client in client_ips:
                 if client and policy['ip_version'] == 4:
                     try:
-                        network = ipaddress.IPv4Network(
-                            policy['src_cidr']
-                        )
+                        network = ipaddress.IPv4Network(policy['src_cidr'])
                         if ipaddress.IPv4Address(client) in network:
                             logger.debug(
                                 'client IPv4: %s matched policy CIDR: %s',
                                 client, policy['src_cidr'])
                             if client_match_policies['CIDR']['hosts'] > 0:
-                                if network.num_addresses < client_match_policies['CIDR']['hosts']:
-                                    client_match_policies['CIDR']['hosts'] = network.num_addresses
-                                    client_match_policies['CIDR']['hashes'] = [policy_hash]
-                                elif network.num_addresses == client_match_policies['CIDR']['hosts']:
-                                    client_match_policies['CIDR']['hashes'].append(policy_hash)
+                                if network.num_addresses < client_match_policies[
+                                        'CIDR']['hosts']:
+                                    client_match_policies['CIDR'][
+                                        'hosts'] = network.num_addresses
+                                    client_match_policies['CIDR']['hashes'] = [
+                                        policy_hash
+                                    ]
+                                elif network.num_addresses == client_match_policies[
+                                        'CIDR']['hosts']:
+                                    client_match_policies['CIDR'][
+                                        'hashes'].append(policy_hash)
                             else:
-                                client_match_policies['CIDR']['hosts'] = network.num_addresses
-                                client_match_policies['CIDR']['hashes'] = [policy_hash]
+                                client_match_policies['CIDR'][
+                                    'hosts'] = network.num_addresses
+                                client_match_policies['CIDR']['hashes'] = [
+                                    policy_hash
+                                ]
                     except:
                         pass
                 elif client and policy['ip_version'] == 6:
                     try:
-                        network = ipaddress.IPv6Network(
-                            policy['src_cidr']
-                        )
+                        network = ipaddress.IPv6Network(policy['src_cidr'])
                         if ipaddress.IPv6Address(client) in network:
                             logger.debug(
                                 'client IPv6: %s matched policy CIDR: %s',
                                 client, policy['src_cidr'])
                             if client_match_policies['CIDR']['hosts'] > 0:
-                                if network.num_addresses < client_match_policies['CIDR']['hosts']:
-                                    client_match_policies['CIDR']['hosts'] = network.num_addresses
-                                    client_match_policies['CIDR']['hashes'] = [policy_hash]
-                                elif network.num_addresses == client_match_policies['CIDR']['hosts']:
-                                    client_match_policies['CIDR']['hashes'].append(policy_hash)
+                                if network.num_addresses < client_match_policies[
+                                        'CIDR']['hosts']:
+                                    client_match_policies['CIDR'][
+                                        'hosts'] = network.num_addresses
+                                    client_match_policies['CIDR']['hashes'] = [
+                                        policy_hash
+                                    ]
+                                elif network.num_addresses == client_match_policies[
+                                        'CIDR']['hosts']:
+                                    client_match_policies['CIDR'][
+                                        'hashes'].append(policy_hash)
                             else:
-                                client_match_policies['CIDR']['hosts'] = network.num_addresses
-                                client_match_policies['CIDR']['hashes'] = [policy_hash]
+                                client_match_policies['CIDR'][
+                                    'hosts'] = network.num_addresses
+                                client_match_policies['CIDR']['hashes'] = [
+                                    policy_hash
+                                ]
                     except:
                         pass
     if client_match_policies['CIDR']['hosts'] > 0:
@@ -459,20 +466,25 @@ def get_matched_policy_hash(request, policy_hashes):
                 header_name = list(header.keys())[0]
                 header_match_value = header[header_name]
                 for header in request.headers.raw:
+                    logger.debug(
+                        'attempting policy match on client request header: %s',
+                        header)
                     if header_name.lower() == header[0].lower():
                         if header_match_value == 'any':
                             logger.debug('header: %s matched any', header_name)
                             header_match = True
                             policy_match_score = policy_match_score + 2
                         elif header_match_value == header[1]:
-                            logger.debug('header: %s matched value exactly', header_name)
+                            logger.debug('header: %s matched value exactly',
+                                         header_name)
                             header_match = True
                             policy_match_score = policy_match_score + 3
                         else:
                             try:
                                 p = re.compile(header_match_value)
                                 if p.match(header[1]):
-                                    logger.debug('header: %s match regex', header_name)
+                                    logger.debug('header: %s match regex',
+                                                 header_name)
                                     header_match = True
                                     policy_match_score = policy_match_score + 3
                             except:
@@ -496,9 +508,8 @@ def get_matched_policy_hash(request, policy_hashes):
             path_regex_match = True
             policy_match_score = policy_match_score + 1
         else:
-            logging.debug(
-                'trying path regex match: %s for %s',
-                policy['path_re_match'], request.url.path)
+            logging.debug('trying path regex match: %s for %s',
+                          policy['path_re_match'], request.url.path)
             if policy['path_re_match']:
                 try:
                     p = re.compile(policy['path_re_match'])
@@ -522,14 +533,16 @@ def apply_policy(client_ips, request, response):
     # Find most specific client IP matching
     client_matched_policy_hashes = get_client_ip_match(client_ips)
     # From matching client IP, find policy attribute matching
-    policy_match_hash = get_matched_policy_hash( request,
-        client_matched_policy_hashes)
+    policy_match_hash = get_matched_policy_hash(request,
+                                                client_matched_policy_hashes)
     policy = policies[policy_match_hash]
     if policy:
         # If matched run reply_scripts
-        logger.debug("%s - matched policy: %s", request.url.path, policy_match_hash)
+        logger.debug("%s - matched policy: %s", request.url.path,
+                     policy_match_hash)
         script_count = len(policies[policy_match_hash]['reply_scripts'])
-        for i, script in enumerate(policies[policy_match_hash]['reply_scripts']):
+        for i, script in enumerate(
+                policies[policy_match_hash]['reply_scripts']):
             script_hash = str(i)
             if script['repeat'] == 0:
                 # run a always matching reply_script
@@ -541,15 +554,16 @@ def apply_policy(client_ips, request, response):
                     policies[policy_match_hash]['repeated'][script_hash] = 0
                 policies[policy_match_hash]['repeated'][script_hash] = (
                     policies[policy_match_hash]['repeated'][script_hash] + 1)
-                if policies[policy_match_hash]['repeated'][script_hash] <= script[
-                        'repeat']:
+                if policies[policy_match_hash]['repeated'][
+                        script_hash] <= script['repeat']:
                     logger.info(
                         "running script count %d of %d" %
                         (policies[policy_match_hash]['repeated'][script_hash],
                          script['repeat']))
-                    if ((i + 1) == script_count) and (
-                            policies[policy_match_hash]['repeated'][script_hash]
-                            == script['repeat']):
+                    if (
+                        (i + 1) == script_count
+                    ) and (policies[policy_match_hash]['repeated'][script_hash]
+                           == script['repeat']):
                         # if this is the last run_script to run and it has
                         # reached it's final count, reset all counts an
                         # start over with new requests.
@@ -559,7 +573,9 @@ def apply_policy(client_ips, request, response):
                     continue
     else:
         logger.error('request did not match any policy')
-        return Response(content='no policy matched', status_code=400, media_type='text/plain')
+        return Response(content='no policy matched',
+                        status_code=400,
+                        media_type='text/plain')
 
 
 @app.get('/logging/', response_class=JSONResponse)
@@ -569,13 +585,16 @@ async def set_level(level: str = 'INFO'):
             logger.warning('setting log level to: %s', level)
             logger.setLevel(logging._nameToLevel[level])
         else:
-            raise HTTPException(status_code=400, detail='Invalid level: %s' % level)
+            raise HTTPException(status_code=400,
+                                detail='Invalid level: %s' % level)
     else:
         level = logger.getEffectiveLevel()
-    return Response(content=json.dumps({ 'log_level': level }), status_code=200)
+    return Response(content=json.dumps({'log_level': level}), status_code=200)
 
 
-@app.get('/policies/', response_model=List[Policy], response_class=JSONResponse)
+@app.get('/policies/',
+         response_model=List[Policy],
+         response_class=JSONResponse)
 async def get_policies():
     return list(policies.values())
 
@@ -660,6 +679,7 @@ async def get_response(path_value: str, request: Request, response: Response):
             for ip in resolve_xff_header(header[1]):
                 if ip not in client_ips:
                     client_ips.append(ip)
+    logger.debug('finding policy for client request from %s', client_ips)
     return apply_policy(client_ips, request, response)
 
 
@@ -672,7 +692,8 @@ async def post_response(path_value: str, request: Request, response: Response):
     for header in request.headers.raw:
         if header[0].lower() == x:
             origin_ip, forward_ip = resolve_xff_header(header[1])
-    return apply_policy([origin_ip, forward_ip, client_host], request, response)
+    return apply_policy([origin_ip, forward_ip, client_host], request,
+                        response)
 
 
 @app.put('/{path_value:path}')
@@ -684,7 +705,8 @@ async def put_response(path_value: str, request: Request, response: Response):
     for header in request.headers.raw:
         if header[0].lower() == x:
             origin_ip, forward_ip = resolve_xff_header(header[1])
-    return apply_policy([origin_ip, forward_ip, client_host], request, response)
+    return apply_policy([origin_ip, forward_ip, client_host], request,
+                        response)
 
 
 @app.patch('/{path_value:path}')
@@ -697,7 +719,8 @@ async def patch_response(path_value: str, request: Request,
     for header in request.headers.raw:
         if header[0].lower() == x:
             origin_ip, forward_ip = resolve_xff_header(header[1])
-    return apply_policy([origin_ip, forward_ip, client_host], request, response)
+    return apply_policy([origin_ip, forward_ip, client_host], request,
+                        response)
 
 
 @app.head('/{path_value:path}')
@@ -709,7 +732,8 @@ async def head_response(path_value: str, request: Request, response: Response):
     for header in request.headers.raw:
         if header[0].lower() == x:
             origin_ip, forward_ip = resolve_xff_header(header[1])
-    return apply_policy([origin_ip, forward_ip, client_host], request, response)
+    return apply_policy([origin_ip, forward_ip, client_host], request,
+                        response)
 
 
 @app.delete('/{path_value:path}')
@@ -722,7 +746,8 @@ async def delete_response(path_value: str, request: Request,
     for header in request.headers.raw:
         if header[0].lower() == x:
             origin_ip, forward_ip = resolve_xff_header(header[1])
-    return apply_policy([origin_ip, forward_ip, client_host], request, response)
+    return apply_policy([origin_ip, forward_ip, client_host], request,
+                        response)
 
 
 @app.options('/{path_value:path}')
@@ -735,4 +760,5 @@ async def options_response(path_value: str, request: Request,
     for header in request.headers.raw:
         if header[0].lower() == x:
             origin_ip, forward_ip = resolve_xff_header(header[1])
-    return apply_policy([origin_ip, forward_ip, client_host], request, response)
+    return apply_policy([origin_ip, forward_ip, client_host], request,
+                        response)
